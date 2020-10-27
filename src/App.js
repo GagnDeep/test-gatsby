b7aa1dfe66a0b8918e5da559d0f58bc8d6169c8c
Gagandeep Singh <35252411+GagnDeep@users.noreply.github.com>
2020-10-27 06:57:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div>
+          <div className="text">
+            le mann b gyi{" "}
+            <span role="img" aria-label="laughling emoji">
+              😂
+            </span>
+          </div>
+
+          <div className="image">
+            <img
+              alt="kaisa lga mera mjak"
+              src="https://i.ytimg.com/vi/DtKfsFlgK7o/hqdefault.jpg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
